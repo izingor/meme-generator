3b32125a7ad7b6c1b974518df1ab4f6e684d2103
Igor <izingor@gmail.com>
2022-02-16 11:26:41 +0200

    third commit- added images

---
+++ js/main.js
@@ -0,0 +1,23 @@
+'use strict'
+
+var gElcanvas;
+var gCtx;
+
+
+
+
+
+function onInit() {
+    gElcanvas = document.querySelector('canvas');
+    gCtx = gElcanvas.getContext('2d');
+}
+
+
+
+
+
+
+
+function renderMeme() {
+
+}
