e5b366839afcca7ad6bac8bddbbd6f8751b1c84b
Igor <izingor@gmail.com>
2022-02-17 11:45:25 +0200

    change adress for images

--- js/services/meme.service.js
+++ js/services/meme.service.js
@@ -3,92 +3,92 @@
 
 const gImgs = [{
         id: 1,
-        url: '/imgs/1.jpg',
+        url: 'imgs/1.jpg',
         keywords: ['trump,politics']
     },
     {
         id: 2,
-        url: '/imgs/2.jpg',
+        url: 'imgs/2.jpg',
         keywords: ['dog,cute,puppy']
     },
     {
         id: 3,
-        url: '/imgs/3.jpg',
+        url: 'imgs/3.jpg',
         keywords: ['baby,pupy,cute']
     },
     {
         id: 4,
-        url: '/imgs/4.jpg',
+        url: 'imgs/4.jpg',
         keywords: ['cat,cute']
     },
     {
         id: 5,
-        url: '/imgs/5.jpg',
+        url: 'imgs/5.jpg',
         keywords: ['baby,success']
     },
     {
         id: 6,
-        url: '/imgs/6.jpg',
+        url: 'imgs/6.jpg',
         keywords: ['aliens,history']
     },
     {
         id: 7,
-        url: '/imgs/7.jpg',
+        url: 'imgs/7.jpg',
         keywords: ['kid,cute,suprised']
     },
     {
         id: 8,
-        url: '/imgs/8.jpg',
+        url: 'imgs/8.jpg',
         keywords: ['wonka,interesting']
     },
     {
         id: 9,
-        url: '/imgs/9.jpg',
+        url: 'imgs/9.jpg',
         keywords: ['kid,evil']
     },
     {
         id: 10,
-        url: '/imgs/10.jpg',
+        url: 'imgs/10.jpg',
         keywords: ['politics,obama']
     },
     {
         id: 11,
-        url: '/imgs/11.jpg',
+        url: 'imgs/11.jpg',
         keywords: ['boxing,kiss,lgbt']
     },
     {
         id: 12,
-        url: '/imgs/12.jpg',
+        url: 'imgs/12.jpg',
         keywords: ['pointing']
     },
     {
         id: 13,
-        url: '/imgs/13.jpg',
+        url: 'imgs/13.jpg',
         keywords: ['dicaprio,congratulations']
     },
     {
         id: 14,
-        url: '/imgs/14.jpg',
+        url: 'imgs/14.jpg',
         keywords: ['matrix,morpheus']
     },
     {
         id: 15,
-        url: '/imgs/15.jpg',
+        url: 'imgs/15.jpg',
         keywords: ['no one, lotr, sean bean']
     },
     {
         id: 16,
-        url: '/imgs/16.jpg',
+        url: 'imgs/16.jpg',
         keywords: ['startrek,kirk,funny']
     },
     {
         id: 17,
-        url: '/imgs/17.jpg',
+        url: 'imgs/17.jpg',
         keywords: ['politics,putin']
     },
     {
         id: 18,
-        url: '/imgs/18.jpg',
+        url: 'imgs/18.jpg',
         keywords: ['toy story,buzz']
     },
 
